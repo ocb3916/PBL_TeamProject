0ccf19d7dad6e1579f72741df17701c349b52ad6
윤기 <129282769+Yun-Gi@users.noreply.github.com>
2024-11-30 02:30:15 +0900

    Update MovieFavoriteListService.java

--- src/main/java/com/example/demo/service/MovieFavoriteListService.java
+++ src/main/java/com/example/demo/service/MovieFavoriteListService.java
@@ -5,13 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.MovieFavoriteListDto;
 import com.example.demo.entity.MovieFavoriteList;
 import com.example.demo.entity.User;
 import com.example.demo.repository.MovieFavoriteListRepository;
+import com.example.demo.repository.UserRepository;
 
 
 @Service
 public class MovieFavoriteListService {
+
+    @Autowired
+    private UserRepository UserRepository;
     
     @Autowired
     private MovieFavoriteListRepository MovieFavoriteListRepository;
