3fd02b361ffc6cf96607d000e647710dff5b07a9
윤기 <129282769+Yun-Gi@users.noreply.github.com>
2024-12-01 21:33:12 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,83 @@
+package com.example.demo.dto;
+
+import java.util.Date;
+
+import com.example.demo.entity.User;
+
+
+public class UserDto {
+    private String id;
+    private String pw;
+    private Date birthDate;
+    private String name;
+    private String nickName;
+    private User.Gender gender;
+    private String email;
+    private String phoneNumber;
+    
+    public UserDto(){}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public User.Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(User.Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
